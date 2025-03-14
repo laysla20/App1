5da658a390af042d72033e26c141b6dc556f123a
láysla-2314290036 <laysandrade73@gmail.com>
2025-03-13 22:42:57 -0300

    atv2

--- MyTime/App.js
+++ MyTime/App.js
@@ -7,23 +7,62 @@ class App extends Component {
       {
         id: 1,
         tarefa: "Ir ao mercado",
+        data: "13/03/2025",
         horario: "14:00",
+        prioridade: "Alta",
+        categoria:"Compras",
         feito: "Não",
         imagem: "https://assets-blog.pagseguro.uol.com.br/wp-content/2022/05/economizar-no-mercado-min.jpg",
       },
       {
         id: 2,
         tarefa: "Lavar as Roupas",
+        data: "13/03/2025",
         horario: "16:30",
+        prioridade: "Alta",
+        categoria:"Casa",
         feito: "Sim",
         imagem: "https://drywashlavanderia.com.br/wp-content/uploads/2023/10/Dicas-para-lavar-roupa-na-maquina-1080x675.jpg",
       },
       {
         id: 3,
         tarefa: "Treino na academia",
+        data: "13/03/2025",
         horario: "18:00",
+        prioridade: "Baixa",
+        categoria:"Saúde",
         feito: "Não",
         imagem: "https://blog.ciaathletica.com.br/wp-content/uploads/2018/08/shutterstock_749969473-1.jpg",
+      },
+      {
+        id: 4,
+        tarefa: "Estudar Programação",
+        data: "13/03/2025",
+        horario: "19:00",
+        prioridade: "Alta",
+        categoria:"Estudo",
+        feito: "Sim",
+        imagem: "https://cdn.unasp.br/blog/wp-content/uploads/2018/02/shutterstock_671632789000.jpg",
+      },
+      {
+        id: 5,
+        tarefa: "Continuar leitura do livro",
+        data: "13/03/2025",
+        horario: "21:00",
+        prioridade: "Baixa",
+        categoria:"Lazer",
+        feito: "Sim",
+        imagem: "https://diariomsnews.com.br/wp-content/uploads/2021/08/mulh.jpg",
+      },
+      {
+        id: 6,
+        tarefa: "Organizar a rotina da semana",
+        data: "16/03/2025",
+        horario: "10:00",
+        prioridade: "Média",
+        categoria:"Pessoal",
+        feito: "Não",
+        imagem: "https://i.ytimg.com/vi/7FoERdIzlNc/maxresdefault.jpg",
       },
     ];
 
@@ -35,9 +74,12 @@ class App extends Component {
             <View key={item.id} style={styles.card}>
               <Image source={{ uri: item.imagem }} style={styles.image} />
               <View style={styles.cardContent}>
-                <Text style={styles.taskTitle}>Tarefa: {item.tarefa}</Text>
+                <Text style={styles.taskDetails}>Tarefa: {item.tarefa}</Text>
                 <Text style={styles.taskDetails}>Horário: {item.horario}</Text>
                 <Text style={styles.taskDetails}>Feito: {item.feito}</Text>
+                <Text style={styles.taskDetails}>Data: {item.data}</Text>
+              <Text style={styles.taskDetails}>Prioridade: {item.prioridade}</Text>
+              <Text style={styles.taskDetails}>Categoria: {item.categoria}</Text>
               </View>
             </View>
           ))}
